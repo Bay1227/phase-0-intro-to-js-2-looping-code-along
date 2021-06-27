8656ff12a424abe0bf451a9f9b57b16ced8389af
Bayartsengel Battulga <tsengelbattulga1227@gmail.com>
2021-06-26 20:23:18 -0700

    Completed for loop

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+const writeCards = ["Aba", "Brendon", "Ali"]
+
+function thankYouCards(writeCards) {
+  for (let i = 0; i < writeCards.length; i++) {
+    console.log(`"Thank you, ${writeCards[i]} for the wonderful birthday gift!"`);
+    debugger;
+   
+  }
+
+  return writeCards;
+}
+
+thankYouCards(writeCards); 
